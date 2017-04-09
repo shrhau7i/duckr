7de0009db0db07de7df453957e36e8e919b3c06e
Brandon Li <brandonli1107@gmail.com>
2017-04-09 23:11:58 +0800

    Added Redux Dev tool to the project

--- app/index.js
+++ app/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import getRoutes from 'config/routes'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import users from 'redux/modules/users'
 import thunk from 'redux-thunk'
 import { checkIfAuthed } from 'helpers/auth'
 
-const store = createStore(users, applyMiddleware(thunk))
+const store = createStore(users, compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : (f) => f
+))
 console.log(store);
 console.log(store.getState());
 
