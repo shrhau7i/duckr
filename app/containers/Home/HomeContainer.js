0195d71c0baf3b0c448877b343034ae3671fe31a
Brandon Li <brandonli1107@gmail.com>
2017-09-16 18:49:17 +0800

    Implement ES6 to HomeContainer

--- app/containers/Home/HomeContainer.js
+++ app/containers/Home/HomeContainer.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Home } from 'components'
 
-const HomeContainer = React.createClass({
-  render () {
-    return (
-      <Home />
-    )
-  },
-})
+// const HomeContainer = React.createClass({
+//   render () {
+//     return (
+//       <Home />
+//     )
+//   },
+// })
+
+const HomeContainer = () => {
+  return (
+    <Home />
+  )
+}
 
 export default HomeContainer
