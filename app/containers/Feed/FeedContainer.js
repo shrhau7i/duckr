3d4171a5d171173b3954a6b4fbe5406a8fc2ad44
Brandon Li <brandonli1107@gmail.com>
2017-10-11 23:53:32 +0800

    Implement ES6 to FeedContainer

--- app/containers/Feed/FeedContainer.js
+++ app/containers/Feed/FeedContainer.js
@@ -4,19 +4,17 @@ import { Feed } from 'components'
 import { bindActionCreators } from 'redux'
 import * as feedActionCreators from 'redux/modules/feed'
 import { List } from 'immutable'
+const { bool, string, func } = PropTypes
 
-const FeedContainer = React.createClass({
-  propTypes: {
-    duckIds: PropTypes.instanceOf(List),
-    newDucksAvailable: PropTypes.bool.isRequired,
-    error: PropTypes.string.isRequired,
-    isFetching: PropTypes.bool.isRequired,
-    setAndHandleFeedListener: PropTypes.func.isRequired,
-    resetNewDucksAvailable: PropTypes.func.isRequired,
-  },
+class FeedContainer extends React.Component {
+  constructor() {
+    super()
+  }
+
   componentDidMount () {
     this.props.setAndHandleFeedListener()
-  },
+  }
+
   render () {
     return (
       <Feed
@@ -26,8 +24,41 @@ const FeedContainer = React.createClass({
         isFetching={this.props.isFetching}
         resetNewDucksAvailable={this.props.resetNewDucksAvailable} />
     )
-  },
-})
+  }
+}
+
+FeedContainer.protoTypes = {
+  duckIds: PropTypes.instanceOf(List),
+  newDucksAvailable: bool.isRequired,
+  error: string.isRequired,
+  isFetching: bool.isRequired,
+  setAndHandleFeedListener: func.isRequired,
+  resetNewDucksAvailable: func.isRequired,
+}
+
+// const FeedContainer = React.createClass({
+//   propTypes: {
+//     duckIds: PropTypes.instanceOf(List),
+//     newDucksAvailable: PropTypes.bool.isRequired,
+//     error: PropTypes.string.isRequired,
+//     isFetching: PropTypes.bool.isRequired,
+//     setAndHandleFeedListener: PropTypes.func.isRequired,
+//     resetNewDucksAvailable: PropTypes.func.isRequired,
+//   },
+//   componentDidMount () {
+//     this.props.setAndHandleFeedListener()
+//   },
+//   render () {
+//     return (
+//       <Feed
+//         duckIds={this.props.duckIds}
+//         newDucksAvailable={this.props.newDucksAvailable}
+//         error={this.props.error}
+//         isFetching={this.props.isFetching}
+//         resetNewDucksAvailable={this.props.resetNewDucksAvailable} />
+//     )
+//   },
+// })
 
 function mapStateToProps ({feed}) {
   return {
