29cb60ee04368d0911857ba09daedd62fbcb0195
Shih-Hao <shihhao.li@rakuten.com>
2017-10-12 23:11:30 +0800

    Implement ES6 to RepliesContainer

--- app/containers/Replies/RepliesContainer.js
+++ app/containers/Replies/RepliesContainer.js
@@ -4,27 +4,19 @@ import { connect } from 'react-redux'
 import { Replies } from 'components'
 import { staleReplies } from 'helpers/utils'
 import * as repliesActionCreators from 'redux/modules/replies'
+const { bool, string, number, object, func } = PropTypes
 
-const RepliesContainer = React.createClass({
-  propTypes: {
-    isFetching: PropTypes.bool.isRequired,
-    error: PropTypes.string.isRequired,
-    lastUpdated: PropTypes.number.isRequired,
-    replies: PropTypes.object,
-    duckId: PropTypes.string.isRequired,
-    fetchAndHandleReplies: PropTypes.func.isRequired,
-  },
-  getDefaultProps () {
-    return {
-      lastUpdated: 0,
-      replies: {},
-    }
-  },
+class RepliesContainer extends React.Component {
+  constructor() {
+    super()
+  }
+
   componentDidMount () {
     if (staleReplies(this.props.lastUpdated)) {
       this.props.fetchAndHandleReplies(this.props.duckId)
     }
-  },
+  }
+
   render () {
     return (
       <Replies
@@ -33,8 +25,52 @@ const RepliesContainer = React.createClass({
         lastUpdated={this.props.lastUpdated}
         replies={this.props.replies} />
     )
-  },
-})
+  }
+}
+
+RepliesContainer.propTypes = {
+  isFetching: bool.isRequired,
+  error: string.isRequired,
+  lastUpdated: number.isRequired,
+  replies: object,
+  duckId: string.isRequired,
+  fetchAndHandleReplies: func.isRequired,
+}
+
+RepliesContainer.defaultProps = {
+  lastUpdated: 0,
+  replies: {},
+}
+// const RepliesContainer = React.createClass({
+//   propTypes: {
+//     isFetching: PropTypes.bool.isRequired,
+//     error: PropTypes.string.isRequired,
+//     lastUpdated: PropTypes.number.isRequired,
+//     replies: PropTypes.object,
+//     duckId: PropTypes.string.isRequired,
+//     fetchAndHandleReplies: PropTypes.func.isRequired,
+//   },
+//   getDefaultProps () {
+//     return {
+//       lastUpdated: 0,
+//       replies: {},
+//     }
+//   },
+//   componentDidMount () {
+//     if (staleReplies(this.props.lastUpdated)) {
+//       this.props.fetchAndHandleReplies(this.props.duckId)
+//     }
+//   },
+//   render () {
+//     return (
+//       <Replies
+//         isFetching={this.props.isFetching}
+//         error={this.props.error}
+//         lastUpdated={this.props.lastUpdated}
+//         replies={this.props.replies} />
+//     )
+//   },
+// })
 
 function mapStateToProps (state, props) {
   const duckRepliesInfo = state.replies[props.duckId] || {}
