eba95cc991bddeb18907c270a24619c3f3c5ca99
Shih-Hao <shihhao.li@rakuten.com>
2017-11-03 13:07:05 +0800

    Used react-i18next and i18next

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -1,11 +1,33 @@
 import React from 'react'
 import { container, title, slogan } from './styles.css'
+import { I18n } from 'react-i18next';
+
+import i18next from 'config/i18n' // initialized i18next instance using reactI18nextModule
 
 export default function Home () {
+  document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('button').addEventListener('click', () => {
+      if (i18next.language != 'en') {
+        i18next.changeLanguage('en')
+      } else {
+        i18next.changeLanguage('cn')
+      }
+    })
+  })
+
   return (
-    <div className={container}>
-      <p className={title}>{'Duckr'}</p>
-      <p className={slogan}>{'The real time, cloud based, modular, scalable, growth hack, social platform. In the cloud.'}</p>
-    </div>
+    <I18n>
+      {
+        (t) => {
+          return (
+            <div className={container}>
+              <p className={title}>{t('title.label')}</p>
+              <p className={slogan}>{t('description.label')}</p>
+              <button>Toggle Language</button>
+            </div>
+          )
+        }
+      }
+    </I18n>
   )
 }
---
+++ app/config/i18n.js
@@ -0,0 +1,29 @@
+import i18next from 'i18next';
+import { reactI18nextModule } from 'react-i18next';
+
+i18next
+  .use(reactI18nextModule)
+  .init({
+    lng: 'en',
+    fallbackLng: 'en',
+    debug: true,
+    react: {
+      wait: true
+    },
+    resources: {
+      en: {
+        translation: {
+          title: { label: 'Duckr', },
+          description: { label: 'The real time, cloud based, modular, scalable, growth hack, social platform. In the cloud.', },
+        },
+      },
+      cn: {
+        translation: {
+          title: { label: '偽推特', },
+          description: { label: '偽推特的社交網站', },
+        },
+      },
+    },
+  });
+
+export default i18next
